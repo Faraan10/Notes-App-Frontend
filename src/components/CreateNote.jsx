abd959373b32fae8a76b7e12dfba6c162389cb6f
Muhammed <faraanshaik19@gmail.com>
2025-04-18 21:25:23 +0530

    updated singlenote and createnote pages

--- src/components/CreateNote.jsx
+++ src/components/CreateNote.jsx
@@ -2,105 +2,111 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { createNotes } from "../redux/slices/notesSlice";
-import { Button, DialogActions, DialogContent, DialogTitle, TextField } from "@mui/material";
+import {
+  Button,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  TextField,
+} from "@mui/material";
 import { toast } from "react-toastify";
 
 const CreateNote = () => {
-	const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-	const [info, setInfo] = useState({
-		title: "",
-		description: "",
-		imageUrl: "",
-	});
+  const [info, setInfo] = useState({
+    title: "",
+    description: "",
+    imageUrl: "",
+  });
 
-	const { title, description, imageUrl } = info;
+  const { title, description, imageUrl } = info;
 
-	const handleChange = (e) => {
-		setInfo({ ...info, [e.target.name]: e.target.value });
-	};
+  const handleChange = (e) => {
+    setInfo({ ...info, [e.target.name]: e.target.value });
+  };
 
-	const handleSubmit = (e) => {
-		e.preventDefault();
-		// console.log(info);
-		if (!imageUrl.match(/^https?:\/\/.*\/.*\.(png|webp|jpeg|jpg)\??.*$/gim)) {
-			toast.error("Enter a valid imageUrl");
-			return;
-		}
-		dispatch(createNotes(info));
-		setInfo({
-			title: "",
-			description: "",
-			imageUrl: "",
-		});
-	};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // console.log(info);
+    if (!imageUrl.match(/^https?:\/\/.*\/.*\.(png|webp|jpeg|jpg)\??.*$/gim)) {
+      toast.error("Enter a valid imageUrl");
+      return;
+    }
+    dispatch(createNotes(info));
+    setInfo({
+      title: "",
+      description: "",
+      imageUrl: "",
+    });
+  };
 
-	return (
-		<>
-			<div className="note_container">
-				<DialogTitle>Create Note</DialogTitle>
-				<form onSubmit={handleSubmit}>
-					<DialogContent>
-						<div className="mb-3">
-							<label htmlFor="title" className="form-label">
-								Title
-							</label>
-							<TextField
-								fullWidth
-								hiddenLabel
-								size="small"
-								name="title"
-								value={title}
-								required
-								placeholder="Enter title here"
-								onChange={handleChange}
-							/>
-						</div>
-						<div className="mb-3">
-							<label htmlFor="description" className="form-label">
-								Description
-							</label>
-							<TextField
-								fullWidth
-								multiline
-								rows={3}
-								name="description"
-								value={description}
-								required
-								placeholder="Enter description here"
-								onChange={handleChange}
-							/>
-						</div>
-						<div className="mb-3">
-							<label htmlFor="tag" className="form-label">
-								Image Url ( Only png, webp, jpeg, jpg will be accepted)
-							</label>
-							<TextField
-								fullWidth
-								hiddenLabel
-								size="small"
-								name="imageUrl"
-								value={imageUrl}
-								required
-								placeholder="Enter image url here"
-								onChange={handleChange}
-							/>
-						</div>
-					</DialogContent>
-					<DialogActions>
-						<Link to="/">
-							<Button variant="contained" color="error">
-								Close
-							</Button>
-						</Link>
-						<Button variant="contained" type="submit" color="success">
-							Add
-						</Button>
-					</DialogActions>
-				</form>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className="note_container">
+        <DialogTitle>Create Note</DialogTitle>
+        <form onSubmit={handleSubmit}>
+          <DialogContent>
+            <div className="mb-3">
+              <label htmlFor="title" className="form-label">
+                Title
+              </label>
+              <TextField
+                fullWidth
+                hiddenLabel
+                size="small"
+                name="title"
+                value={title}
+                required
+                placeholder="Enter title here"
+                onChange={handleChange}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="description" className="form-label">
+                Description
+              </label>
+              <TextField
+                fullWidth
+                multiline
+                rows={3}
+                name="description"
+                value={description}
+                required
+                placeholder="Enter description here"
+                onChange={handleChange}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="tag" className="form-label">
+                Image Url ( Only png, webp, jpeg, jpg will be accepted)
+              </label>
+              <TextField
+                fullWidth
+                hiddenLabel
+                size="small"
+                name="imageUrl"
+                value={imageUrl}
+                required
+                placeholder="Enter image url here"
+                onChange={handleChange}
+              />
+            </div>
+          </DialogContent>
+          <DialogActions>
+            <Link to="/dashboard">
+              <Button variant="contained" color="error">
+                Close
+              </Button>
+            </Link>
+            <Button variant="contained" type="submit" color="success">
+              Add
+            </Button>
+          </DialogActions>
+        </form>
+      </div>
+    </>
+  );
 };
 
 export default CreateNote;
--- src/components/SingleNote.jsx
+++ src/components/SingleNote.jsx
@@ -11,42 +11,54 @@ import getSingleNote from "../redux/api/getSingleNote";
 import { Link } from "react-router-dom";
 
 const SingleNote = () => {
-	const [data, setData] = useState();
-	const params = useParams();
-	const id = params.id;
+  const [data, setData] = useState();
+  const params = useParams();
+  const id = params.id;
 
-	const getData = async () => {
-		const response = await getSingleNote(id);
-		setData(response);
-	};
+  const getData = async () => {
+    const response = await getSingleNote(id);
+    setData(response);
+  };
 
-	useEffect(() => {
-		getData();
-	}, []);
+  useEffect(() => {
+    getData();
+  }, []);
 
-	// console.log(data?.imageUrl);
-	return (
-		<div style={{ display: "flex", alignItems: "center", justifyContent: "center", padding: "20px" }}>
-			<Card sx={{ maxWidth: 900 }}>
-				<CardMedia component="img" alt="image not working" height="100%" image={data?.imageUrl} />
-				<CardContent>
-					<Typography gutterBottom variant="h5" component="div">
-						{data?.title}
-					</Typography>
-					<Typography variant="body2" color="text.secondary">
-						{data?.description}
-					</Typography>
-				</CardContent>
-				<CardActions>
-					<Link to="/">
-						<Button variant="contained" color="error">
-							Close
-						</Button>
-					</Link>
-				</CardActions>
-			</Card>
-		</div>
-	);
+  // console.log(data?.imageUrl);
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        padding: "20px",
+      }}
+    >
+      <Card sx={{ maxWidth: 900 }}>
+        <CardMedia
+          component="img"
+          alt="image not working"
+          height="100%"
+          image={data?.imageUrl}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {data?.title}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {data?.description}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Link to="/dashboard">
+            <Button variant="contained" color="error">
+              Close
+            </Button>
+          </Link>
+        </CardActions>
+      </Card>
+    </div>
+  );
 };
 
 export default SingleNote;
